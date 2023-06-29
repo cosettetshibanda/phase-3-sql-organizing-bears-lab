4b30032722d4dbc3efa527180d044c11face6f52
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-06-28 19:13:49 -0600

    commit tree 568267421d0adc01dfd1e0aa0c3d1944702a47fc

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "brown", "mild", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 8, "M", "orange", "wild", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 7, "F", "white", "fiesty", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, "M", "brown", "mild", 0);
+INSERT INTO bears VALUES (5, "Melissa", 15, "F", "orange", "mild", 1);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "gray", "fiesty", 1);
+INSERT INTO bears VALUES (7, "Wendy", 3, "F", "white", "wild", 0);
+INSERT INTO bears VALUES (8, NULL, 5, "F", "gray", "mild", 1);
